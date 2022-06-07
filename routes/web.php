cdf0051deaf1f7900a1d52f194ad630c9cde9912
Ga <gabri.magni.97@gmail.com>
2022-06-07 17:16:21 +0200

    push

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+->prefix('admin')
+->name('admin.')
+->namespace('Admin')
+->group( function(){
+    Route::get('/admin', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+});
